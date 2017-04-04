8b5d68c2f00f5ab0d268b37370d9334453e4982d
Huang Bo <huangbop@gmail.com>
2017-04-04 16:36:31 +0800

    With get/post

--- webhooks/views.py
+++ webhooks/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class Webhook(TemplateView):
     template_name = "_www/index.html"
+
+    def get(self, request, *args, **kwargs):
+        return super(Webhook, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super(Webhook, self).post(request, *args, **kwargs)
