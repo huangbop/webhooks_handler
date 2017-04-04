52cb3928ccdb76e6842ad37812ac56bd48e7fc7f
Huang Bo <huangbop@gmail.com>
2017-04-04 21:28:42 +0800

    Deploy

--- webhooks/views.py
+++ webhooks/views.py
@@ -1,9 +1,10 @@
 from django.views.generic import TemplateView
 import json
+import subprocess
 
 
 class Webhook(TemplateView):
-    template_name = "_www/index.html"
+    template_name = "index.html"
 
     def get(self, request, *args, **kwargs):
         return super(Webhook, self).get(request, *args, **kwargs)
@@ -12,4 +13,6 @@ class Webhook(TemplateView):
         d = json.loads(request.body.decode())
         if d['ref'] == 'refs/heads/master':
             print('Do pull & deploy')
+            subprocess.call("git submodule update --remote")
+            subprocess.call("cd ../FW_Docs2 & make html")
         return super(Webhook, self).post(request, *args, **kwargs)
